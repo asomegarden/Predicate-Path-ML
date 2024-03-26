fd489dca6036b713dfda6875d9010ce3f8d3022f
이정원 <won31080@gmail.com>
2024-03-26 21:41:14 +0900

    save: 보상 시스템 구성 후 학습(결과-실패)
    대담성의 영향이 더욱 커질 필요가 있음

--- Assets/Scripts/ThiefLearn/ThiefAgent.cs
+++ Assets/Scripts/ThiefLearn/ThiefAgent.cs
@@ -7,20 +7,24 @@ using Unity.MLAgents.Sensors;
 using Unity.VisualScripting;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThiefAgent : Agent
 {
-    public int[] boldnesses = { 0, 10, 30, 40 };
-    private int boldness = 10;
-
-    private int defaultPenalty = -20;
+    public int[] boldnesses = { 0, 5, 10, 15 };
+    public int boldness = 10;
 
     public int currentPoint = 0;
 
     public WayPoint[] wayPoints;
     private WayPoint destinationWaypoint;
+    private int wayPointIndex;
+
+    public Text blodnessText;
 
     public float moveSpeed = 4f;
+
+    public List<WayPoint> prevWayPoints = new List<WayPoint>();
 
     private void Awake()
     {
@@ -29,15 +33,17 @@ public class ThiefAgent : Agent
 
     public override void Initialize()
     {
-
+        blodnessText.text = $"blodness {boldness}";
     }
 
 
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(boldness); //대담성 추가
+        sensor.AddObservation(currentPoint);
+        sensor.AddObservation(wayPointIndex);
 
-        foreach(var wayPoint in wayPoints) //방문지 정보 추가
+        foreach (var wayPoint in wayPoints) //방문지 정보 추가
         {
             Vector3 position = wayPoint.transform.localPosition;
             sensor.AddObservation(position.x);
@@ -50,14 +56,27 @@ public class ThiefAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        int wayPointIndex = actions.DiscreteActions[0];
+        wayPointIndex = actions.DiscreteActions[0];
         destinationWaypoint = wayPoints[wayPointIndex];
 
-        StartCoroutine(MovePathCoroutine());
+        if (prevWayPoints.Contains(destinationWaypoint))
+        {
+            AddReward(-50); // 중복 방문지 패널티: 큼
+            RequestDecision();
+        }
+        else
+        {
+            prevWayPoints.Add(destinationWaypoint);
+            StartCoroutine(MovePathCoroutine());
+        }
     }
 
     public override void OnEpisodeBegin()
     {
+        prevWayPoints.Clear();
+        //boldness = boldnesses[Random.Range(0, boldnesses.Length)];
+        
+        currentPoint = 0;
         transform.localPosition = new Vector3(-1, -1, 0);
         RequestDecision();
     }
@@ -84,18 +103,35 @@ public class ThiefAgent : Agent
 
     private void UseWayPoint()
     {
-        // 보상 계산
-        AddReward(-destinationWaypoint.risk);
-        AddReward(destinationWaypoint.point);
+        // 보상 계산\
+        // 고려할 파라미터 목록
+        // 현재 위치와의 거리: 멀수록 패널티 (0~20)
+        // 획득할 수 있는 포인트 (0~10) //이건 나중에 방문지 도착할 때 한번에 제공하는걸로.
+        // 방문지 리스크 (0~20)
+        // 방문지 리스크는 boldness에 따라 달라짐. 이때 boldness에 따라 선택이 크게 달라질 수 있어야함
 
-        RequestDecision();
+        Vector3 wayPointPosition = destinationWaypoint.transform.localPosition;
 
+        float xDistance = Mathf.Abs(wayPointPosition.x - transform.localPosition.x);
+        float yDistance = Mathf.Abs(wayPointPosition.y - transform.localPosition.y);
+        float distancePenalty = (xDistance + yDistance);
+
+        float reward = 0f;
+        reward -= distancePenalty;
+        reward -= (boldness - destinationWaypoint.risk); //리스크를 인지하는 정도 완화
+
+        currentPoint += destinationWaypoint.point;
 
         // 에피소드 종료
         if (destinationWaypoint == wayPoints[wayPoints.Length - 1])
         {
-            AddReward(100);
+            AddReward(currentPoint);
             EndEpisode();
+        }
+        else
+        {
+            AddReward(reward);
+            RequestDecision();
         }
     }
 }
