14ec9a5d83f0ab46797f247674b86d6d78401226
이정원 <won31080@gmail.com>
2024-05-05 23:02:45 +0900

    save: 다수의 경찰을 이용한 예측 테스트

--- Assets/Scripts/PoliceLearn/PoliceSystemAgent.cs
+++ Assets/Scripts/PoliceLearn/PoliceSystemAgent.cs
@@ -15,8 +15,9 @@ public class PoliceSystemAgent : Agent
     private float penaltyForMissingThief = -10;
 
     public Thief thief;
-    public GameObject policeObject;
     public Text logText;
+
+    public GameObject[] policeObjects;
 
     private Vector2 prevPosition = Vector2.zero;
 
@@ -32,28 +33,42 @@ public class PoliceSystemAgent : Agent
 
     public void OnFeedEaten(Vector3 position)
     {
+        prevPosition = eatenPosition;
         eatenPosition = position;
-        float xDistance = Mathf.Abs(eatenPosition.x - policeObject.transform.localPosition.x);
-        float yDistance = Mathf.Abs(eatenPosition.y - policeObject.transform.localPosition.y);
-        float distancePenalty = (xDistance + yDistance);
-        float penalty = (penaltyForMissingThief - distancePenalty);
-        SetReward(penalty - ++step);
-        // 기본 패널티
-        // 거리에 따른 패널티
-        // 뒤 스탭으로 갈수록 패널티
-        // 1:1:1 비율이 되기를 원함
-        // 경로가 최대 18정도로 구성되어있고, 거리도 최대 20이기 때문에, 같은 비율을 가짐.
-        // 기본 패널티는 그 절반정도인 10으로 구성
-        // 그러면 다 합쳐서 50정도가 최대 패널티
-        // 그러면 보상은 그 5배에서 10배정도로 구성해서 250~500으로 구성
-        // 현재는 300으로 설정
-        RequestDecision();
+
+            float xDistance = 0; 
+            float yDistance = 0; 
+
+            foreach (var policeObject in policeObjects)
+            {
+                xDistance += Mathf.Abs(eatenPosition.x - policeObject.transform.localPosition.x);
+                yDistance += Mathf.Abs(eatenPosition.y - policeObject.transform.localPosition.y);
+            }
+
+            xDistance /= policeObjects.Length;
+            yDistance /= policeObjects.Length;
+
+            float distancePenalty = (xDistance + yDistance);
+            float penalty = (penaltyForMissingThief - distancePenalty);
+            SetReward(penalty - ++step);
+            // 기본 패널티
+            // 거리에 따른 패널티
+            // 뒤 스탭으로 갈수록 패널티
+            // 1:1:1 비율이 되기를 원함
+            // 경로가 최대 18정도로 구성되어있고, 거리도 최대 20이기 때문에, 같은 비율을 가짐.
+            // 기본 패널티는 그 절반정도인 10으로 구성
+            // 그러면 다 합쳐서 50정도가 최대 패널티
+            // 그러면 보상은 그 5배에서 10배정도로 구성해서 250~500으로 구성
+            // 현재는 300으로 설정
+            RequestDecision();
     }
 
     public void OnThiefCaught()
     {
         SetReward(rewardForCatchingThief);
         PrintLog($"catch");
+
+        //RequestDecision();
     }
 
     public override void Initialize()
@@ -61,7 +76,7 @@ public class PoliceSystemAgent : Agent
         thief.Initialize();
         eatenPosition = Vector3.zero;
         prevPosition = Vector3.zero;
-        policeObject.transform.localPosition = new Vector3(-2, -1, 0);
+        foreach (var policeObject in policeObjects) policeObject.transform.localPosition = new Vector3(-2, -1, 0);
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -72,46 +87,52 @@ public class PoliceSystemAgent : Agent
 
         sensor.AddObservation(direction.x);
         sensor.AddObservation(direction.y);
-
-        prevPosition = eatenPosition;
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        int xPos = actions.DiscreteActions[0];
-        int yPos = actions.DiscreteActions[1];
-
-        Vector2 movePosition = new Vector3(xPos - 5 + eatenPosition.x, yPos - 5 + eatenPosition.y, 0);
-
-        //Clamp 과정
-        if(movePosition.x > 9)
+        for (int i = 0; i < policeObjects.Length; i++)
         {
-            movePosition.x = 9;
-        }else if(movePosition.x < 0)
-        {
-            movePosition.x = 0;
+            int xPos = actions.DiscreteActions[i];
+            int yPos = actions.DiscreteActions[i+1];
+
+            Vector2 movePosition = new Vector3(xPos - 5 + eatenPosition.x, yPos - 5 + eatenPosition.y, 0);
+
+            //Clamp 과정
+            if (movePosition.x > 9)
+            {
+                movePosition.x = 9;
+            }
+            else if (movePosition.x < 0)
+            {
+                movePosition.x = 0;
+            }
+
+            if (movePosition.y > 9)
+            {
+                movePosition.y = 9;
+            }
+            else if (movePosition.y < 0)
+            {
+                movePosition.y = 0;
+            }
+
+            policeObjects[i].transform.localPosition = movePosition;
         }
-
-        if(movePosition.y > 9)
-        {
-            movePosition.y = 9;
-        }else if(movePosition.y < 0)
-        {
-            movePosition.y = 0;
-        }
-
-        policeObject.transform.localPosition = movePosition;
     }
 
     public override void OnEpisodeBegin()
     {
         step = 0;
+
         thief.OnEpisodeBegin();
         ClearLog();
         PrintLog($"start episode {episodeCount}");
         episodeCount++;
         eatenPosition = Vector3.zero;
-        policeObject.transform.localPosition = new Vector3(-2, -1, 0);
+
+        foreach(var policeObject in policeObjects) 
+            policeObject.transform.localPosition = new Vector3(-2, -1, 0);
     }
 
     private void PrintLog(string content)
